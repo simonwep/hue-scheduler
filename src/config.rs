b38d90c6611fc841a5d6d1b42371c16bff5f2333
Simon Reinisch <simon@reinisch.io>
2023-11-22 17:23:30 +0100

    Ignore missing .env files

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::net::IpAddr;
 use std::str::FromStr;
 use std::time::Duration;
@@ -10,14 +11,16 @@ pub struct Config {
 }
 
 pub fn load_config() -> Config {
-    dotenv::dotenv().expect("failed to load .env variables");
+    if dotenv::dotenv().is_err() {
+        println!("No .env file found");
+    }
 
-    let bridge_username = dotenv::var("BRIDGE_USERNAME").expect("BRIDGE_USERNAME missing");
-    let bridge_raw_addr = dotenv::var("BRIDGE_IP").expect("BRIDGE_IP missing");
+    let bridge_username = env::var("BRIDGE_USERNAME").expect("BRIDGE_USERNAME missing");
+    let bridge_raw_addr = env::var("BRIDGE_IP").expect("BRIDGE_IP missing");
     let bridge_ip = IpAddr::from_str(bridge_raw_addr.as_str()).expect("failed to parse BRIDGE_IP");
 
     let interval = Duration::from_millis(
-        dotenv::var("INTERVAL")
+        env::var("INTERVAL")
             .expect("INTERVAL missing")
             .parse::<u64>()
             .expect("failed to parse INTERVAL"),
