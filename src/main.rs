66ee69a606357812c17d00f4dfe9decf7ca84905
Simon Reinisch <simon@reinisch.io>
2023-11-23 17:12:18 +0100

    Improve algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::utils::get_scheduled_scenes;
 use huelib2::resource::group::StateModifier;
+use huelib2::resource::{Light, Scene};
 use huelib2::Bridge;
 use std::collections::HashMap;
 
@@ -13,67 +14,85 @@ fn main() {
     let mut reachable = HashMap::<String, bool>::new();
 
     loop {
-        std::thread::sleep(configuration.interval);
+        if !reachable.is_empty() {
+            std::thread::sleep(configuration.interval);
+        }
 
         let Ok(all_lights) = bridge.get_all_lights() else {
             eprintln!("Failed to retrieve lights");
             continue;
         };
 
-        let Ok(all_scenes) = bridge
-            .get_all_scenes()
-            .map_err(|_| "Failed to retrieve scenes")
-        else {
+        // Check for light changes
+        let changed_lights = all_lights
+            .into_iter()
+            .filter(|light| {
+                let Some(last_reachable) = reachable.get(&light.id) else {
+                    return true;
+                };
+                *last_reachable != light.state.reachable
+            })
+            .collect::<Vec<Light>>();
+
+        if changed_lights.is_empty() {
+            continue;
+        }
+
+        if reachable.is_empty() {
+            for light in changed_lights.iter() {
+                reachable.insert(light.id.clone(), light.state.reachable);
+            }
+            continue;
+        }
+
+        // Update reachable lights
+        for light in changed_lights.iter() {
+            if let Some(last_reachable) = reachable.get(&light.id) {
+                if *last_reachable && !light.state.reachable {
+                    println!("Light \"{}\" is not reachable anymore", light.name)
+                } else {
+                    println!("Light \"{}\" is reachable again", light.name);
+                };
+            };
+
+            reachable.insert(light.id.clone(), light.state.reachable);
+        }
+
+        // Check for scene changes
+        let changed_light_ids = changed_lights
+            .iter()
+            .map(|light| light.id.clone())
+            .collect::<Vec<String>>();
+
+        let Ok(changed_scenes) = bridge.get_all_scenes().map(|scenes| {
+            scenes
+                .into_iter()
+                .filter(|scene| {
+                    scene
+                        .lights
+                        .clone()
+                        .map(|light_ids| {
+                            light_ids
+                                .iter()
+                                .all(|light_id| changed_light_ids.contains(&light_id))
+                        })
+                        .unwrap_or(false)
+                })
+                .collect::<Vec<Scene>>()
+        }) else {
             eprintln!("Failed to retrieve scenes");
             continue;
         };
 
-        for scheduled_scene in get_scheduled_scenes(&all_scenes).iter() {
-            let Some(scene) = all_scenes
-                .iter()
-                .find(|scene| scene.id == scheduled_scene.scene_id)
-            else {
-                eprintln!("Scene not found: {}", scheduled_scene.scene_id);
+        // Process scenes
+        for scheduled_scene in get_scheduled_scenes(&changed_scenes).iter() {
+            if let Err(err) = bridge.set_group_state(
+                &scheduled_scene.scene_id,
+                &StateModifier::new().with_scene(scheduled_scene.scene_id.clone()),
+            ) {
+                eprintln!("Failed to set scene: {}", err);
                 continue;
-            };
-
-            let Some(group_id) = &scene.group else {
-                eprintln!("Scene has no group: {}", scene.name);
-                continue;
-            };
-
-            let Some(lights) = &scene.lights else {
-                eprintln!("Scene has no lights: {}", scene.name);
-                continue;
-            };
-
-            let turn_on = lights.iter().all(|light_id| {
-                let Some(light) = all_lights.iter().find(|light| light.id.eq(light_id)) else {
-                    eprintln!("Light not found: {}", light_id);
-                    return false;
-                };
-
-                let Some(last_reachable) = reachable.get(light_id) else {
-                    return false;
-                };
-
-                return !*last_reachable && light.state.reachable;
-            });
-
-            if turn_on {
-                let modifier = StateModifier::new().with_scene(scene.id.clone());
-
-                if let Err(err) = bridge.set_group_state(group_id.clone(), &modifier) {
-                    println!("Failed to set scene: {} for {}", err, scene.name);
-                } else {
-                    println!("Set scene: {}", scene.name);
-                }
             }
-        }
-
-        // Update light states
-        for light in all_lights.iter() {
-            reachable.insert(light.id.clone(), light.state.reachable);
         }
     }
 }
